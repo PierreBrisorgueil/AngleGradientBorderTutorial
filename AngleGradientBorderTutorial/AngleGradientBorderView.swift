dcc5ffc823ecbda15557308314a3173ff898ea62
Pierre <pierrebrisorgueil@me.com>
2015-04-29 23:05:56 +0200

    Add image / edit animations

--- AngleGradientBorderTutorial/AngleGradientBorderView.swift
+++ AngleGradientBorderTutorial/AngleGradientBorderView.swift
@@ -39,7 +39,7 @@ class AngleGradientBorderView: UIView {
   // Setup the attributes of this view's layer
   func setupGradientLayer(borderColors gradientBorderColors: [AnyObject]? = nil, borderWidth gradientBorderWidth: CGFloat? = nil) {
     // Grab this UIView's layer and cast it as AngleGradientBorderLayer
-    let l: AngleGradientBorderLayer = self.layer as AngleGradientBorderLayer
+    let l: AngleGradientBorderLayer = self.layer as! AngleGradientBorderLayer
 
     // NOTE: Since our gradient layer is built as an image,
     // we need to scale it to match the display of the device.
--- AngleGradientBorderTutorial/ViewController.swift
+++ AngleGradientBorderTutorial/ViewController.swift
@@ -9,99 +9,126 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  // UI
-  @IBOutlet var angleGradientBorderView1: AngleGradientBorderView!
-  @IBOutlet var angleGradientBorderView2: AngleGradientBorderView!
-  @IBOutlet var angleGradientBorderView3: AngleGradientBorderView!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    let angleGradientBorderView1Colors: [AnyObject] = [
-      UIColor.purpleColor().CGColor,
-      UIColor.yellowColor().CGColor,
-      UIColor.orangeColor().CGColor,
-      UIColor.purpleColor().CGColor,
-    ]
-    // Change the colors of angleGradientBorderView1 and its border width
-    angleGradientBorderView1.setupGradientLayer(borderColors: angleGradientBorderView1Colors, borderWidth: 5)
-
-    let angleGradientBorderView3Colors: [AnyObject] = [
-      UIColor.redColor().CGColor,
-      UIColor.orangeColor().CGColor,
-      UIColor.yellowColor().CGColor,
-      UIColor.greenColor().CGColor,
-      UIColor.blueColor().CGColor,
-      UIColor.purpleColor().CGColor,
-      UIColor.magentaColor().CGColor,
-      UIColor.redColor().CGColor,
-    ]
-    // Change the colors of angleGradientBorderView1 and its border width
-    angleGradientBorderView3.setupGradientLayer(borderColors: angleGradientBorderView3Colors, borderWidth: 10)
-
-    // Add some animation
-    scaleDownView()
-    rotateView()
-  }
-
-  func scaleDownView() {
-    let animation = CABasicAnimation(keyPath: "transform.scale")
-    animation.fromValue = 1
-    animation.toValue = 0.5
-    animation.duration = 2
-    animation.removedOnCompletion = true
-    animation.delegate = self
-    animation.setValue("shrink", forKey: "animationId")
-    angleGradientBorderView1.layer.addAnimation(animation, forKey: "shrink")
-  }
-
-  func scaleUpView() {
-    let animation = CABasicAnimation(keyPath: "transform.scale")
-    animation.fromValue = 0.5
-    animation.toValue = 1
-    animation.duration = 2
-    animation.removedOnCompletion = true
-    animation.delegate = self
-    animation.setValue("grow", forKey: "animationId")
-    angleGradientBorderView1.layer.addAnimation(animation, forKey: "grow")
-  }
-
-  func rotateView() {
-    let animation = CABasicAnimation(keyPath: "transform.rotation")
-    animation.fromValue = 0
-    animation.toValue = 2 * M_PI
-    animation.duration = 3
-    animation.removedOnCompletion = true
-    animation.delegate = self
-    animation.setValue("spin", forKey: "animationId")
-    angleGradientBorderView3.layer.addAnimation(animation, forKey: "spin")
-  }
-
-  override func animationDidStop(anim: CAAnimation!, finished flag: Bool) {
-    if flag {
-      if let id: String = anim.valueForKey("animationId") as? String {
-        switch id {
-        case "shrink":
-          scaleUpView()
-
-        case "grow":
-          scaleDownView()
-
-        case "spin":
-          rotateView()
-
-        default:
-          break
-        }
-      }
+    
+    // UI
+    @IBOutlet var angleGradientBorderView1: AngleGradientBorderView!
+    @IBOutlet var angleGradientBorderView2: AngleGradientBorderView!
+    @IBOutlet var angleGradientBorderView3: AngleGradientBorderView!
+    
+    @IBOutlet weak var imgWaos1: UIImageView!
+    @IBOutlet weak var imgWaos2: UIImageView!
+    @IBOutlet weak var imgWaos3: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.imgWaos1.layer.cornerRadius = self.imgWaos1.frame.size.width / 2
+        self.imgWaos1.clipsToBounds = true
+        
+        self.imgWaos2.layer.cornerRadius = self.imgWaos2.frame.size.width / 2
+        self.imgWaos2.clipsToBounds = true
+        
+        self.imgWaos3.layer.cornerRadius = self.imgWaos3.frame.size.width / 2
+        self.imgWaos3.clipsToBounds = true
+        
+        let angleGradientBorderView1Colors: [AnyObject] = [
+            UIColor.redColor().CGColor,
+            UIColor.orangeColor().CGColor,
+            UIColor.yellowColor().CGColor,
+            UIColor.greenColor().CGColor,
+            UIColor.blueColor().CGColor,
+            UIColor.purpleColor().CGColor,
+            UIColor.magentaColor().CGColor,
+            UIColor.redColor().CGColor,
+        ]
+        // Change the colors of angleGradientBorderView1 and its border width
+        angleGradientBorderView1.setupGradientLayer(borderColors: angleGradientBorderView1Colors, borderWidth: 5)
+        
+        let angleGradientBorderView3Colors: [AnyObject] = [
+            UIColor.redColor().CGColor,
+            UIColor.orangeColor().CGColor,
+            UIColor.yellowColor().CGColor,
+            UIColor.greenColor().CGColor,
+            UIColor.blueColor().CGColor,
+            UIColor.purpleColor().CGColor,
+            UIColor.magentaColor().CGColor,
+            UIColor.redColor().CGColor,
+        ]
+        
+        angleGradientBorderView2.center.x -= view.bounds.width
+        
+        
+        // Three annimations
+        CATransaction.begin()
+        CATransaction.setCompletionBlock({
+            CATransaction.begin()
+            CATransaction.setCompletionBlock({
+                self.scaleView(self.angleGradientBorderView1, f: 1.1, t: 1, d: 0.1)
+            })
+            self.scaleView(self.angleGradientBorderView1, f: 0.8, t: 1.1, d: 0.15)
+            CATransaction.commit()
+        })
+        scaleView(angleGradientBorderView1, f: 1.2, t: 0.8, d: 0.1)
+        CATransaction.commit()
+        // Change the colors of angleGradientBorderView1 and its border width
+        angleGradientBorderView3.setupGradientLayer(borderColors: angleGradientBorderView3Colors, borderWidth: 5)
     }
-  }
+    
+    
+    @IBAction func action2(sender: AnyObject) {
+        shakeView(angleGradientBorderView2, x: 5, y: 0, d: 0.05, r: 5)
+    }
+    
+    @IBAction func action3(sender: AnyObject) {
+        rotateView(angleGradientBorderView3, f: 0, t: 2 * M_PI, d: 0.25)
+    }
+    
+    
+    func scaleView(obj: AngleGradientBorderView, f: Double, t: Double, d: Double) {
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        animation.fromValue = f
+        animation.toValue = t
+        animation.duration = d
+        animation.removedOnCompletion = true
+        animation.delegate = self
+        animation.setValue("shrink", forKey: "animationId")
+        obj.layer.addAnimation(animation, forKey: "shrink")
+    }
+    
+    func shakeView(obj: AngleGradientBorderView, x: CGFloat, y: CGFloat, d: Double, r: Float){
+        var shake:CABasicAnimation = CABasicAnimation(keyPath: "position")
+        shake.duration = d
+        shake.repeatCount = r
+        shake.autoreverses = true
+    
+        var from_point:CGPoint = CGPointMake(self.view.center.x - x, self.view.center.y - y)
+        var from_value:NSValue = NSValue(CGPoint: from_point)
+        
+        var to_point:CGPoint = CGPointMake(self.view.center.x + x, self.view.center.y + y)
+        var to_value:NSValue = NSValue(CGPoint: to_point)
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+        shake.fromValue = from_value
+        shake.toValue = to_value
+        obj.layer.addAnimation(shake, forKey: "position")
+    }
+    
+    func rotateView(obj: AngleGradientBorderView, f: Double, t: Double, d: Double) {
+        let animation = CABasicAnimation(keyPath: "transform.rotation")
+        animation.fromValue = f
+        animation.toValue = t
+        animation.duration = d
+        animation.removedOnCompletion = true
+        animation.delegate = self
+        animation.setValue("spin", forKey: "animationId")
+        obj.layer.addAnimation(animation, forKey: "spin")
+    }
 
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 }
 
